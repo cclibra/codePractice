0db24f2e1c04f64464d073e4f53673436b14c912
cclibra <469284351@qq.com>
2020-02-22 14:19:58 +0800

    [MOD] leetcode111 求二叉树的最小深度 注释

--- src/leetcode/TreeNode/leetcode111_minDepth.java
+++ src/leetcode/TreeNode/leetcode111_minDepth.java
@@ -5,6 +5,9 @@ package leetcode.TreeNode;
  * 给定一个二叉树，找出其最小深度。
  * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
  * 说明: 叶子节点是指没有子节点的节点。
+ * 递归：当节点为空时，跳出递归。
+ * 每次需要做的就是计算左右节点的深度，然后将较小的值+1作为深度返回。
+ * 这里要注意，如果左右节点有空值，应该讲两部分求和，不应该将较小的0值返回
  */
 public class leetcode111_minDepth {
     public int minDepth(TreeNode head) {
